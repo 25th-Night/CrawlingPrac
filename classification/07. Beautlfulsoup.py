e096acaba084a3490d50fd71cce7d0af04ab71fe
25th-Night <brown_eyed87@naver.com>
2023-06-25 08:04:19 +0900

    07. Beautifulsoup - 네이버 환율

--- classification/07. Beautlfulsoup.py
+++ classification/07. Beautlfulsoup.py
@@ -2,16 +2,36 @@ from bs4 import BeautifulSoup as BS
 import requests as req
 
 
-url = "https://naver.com"
+url = "https://finance.naver.com/marketindex/?tabSel=exchange#tab_section"
 res = req.get(url)
 
-title = res.text.split("<title>")[1].split("</title>")[0]
-print(title)
-# NAVER
+soup = BS(res.text, "html.parser")
+
+tds = soup.find_all("td")
+print(tds)
+# 원하는 결과가 나오지 않음
+
+
+url = "https://finance.naver.com/marketindex/exchangeList.naver"
+res = req.get(url)
 
 soup = BS(res.text, "html.parser")
-print(soup.title)
-# <title>NAVER</title>
 
-print(soup.title.string)
-# NAVER
+tds = soup.find_all("td")
+
+names = []
+for td in tds:
+    if len(td.find_all('a')) == 0:
+        continue
+    names.append(td.get_text(strip=True))
+
+prices = []
+for td in tds:
+    if "class" in td.attrs and "sale" in td.attrs["class"]:
+        prices.append(td.get_text(strip=True))
+
+print(names)
+print(prices)
+
+for name, price in zip(names, prices):
+    print(name, price)
