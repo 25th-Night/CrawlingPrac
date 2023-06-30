e5cb6e14bf0b91f056aaf66f0f99fdf251bc83fe
25th-Night <brown_eyed87@naver.com>
2023-06-30 10:04:53 +0900

    14. webhook.site 실습 - GET 요청

---
+++ classification/14. webhook.site 실습.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup as BS
+import requests as req
+
+# python "classification/14. webhook.site 실습.py"
+
+# GET 요청
+
+url = "https://webhook.site/0603ae22-be20-4df5-8929-d265c6af8752"
+querystring = "?name=csw&message=hi"
+headers = {"User-Agent": "csw"}
+res = req.get(url + querystring, headers=headers)
+print(res.status_code)      # 200
+print(res.text)             # ""
