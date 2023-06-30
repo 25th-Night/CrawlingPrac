8e619dc4945c8b449533dc6e6c453a5b15851287
25th-Night <brown_eyed87@naver.com>
2023-06-30 14:31:31 +0900

    14. webhook.site 실습 - POST 요청

--- classification/14. webhook.site 실습.py
+++ classification/14. webhook.site 실습.py
@@ -5,9 +5,24 @@ import requests as req
 
 # GET 요청
 
+# url = "https://webhook.site/0603ae22-be20-4df5-8929-d265c6af8752"
+# querystring = "?name=csw&message=hi"
+# headers = {"User-Agent": "csw"}
+# res = req.get(url + querystring, headers=headers)
+# print(res.status_code)      # 200
+# print(res.text)             # ""
+
+
+# -----------------------------------------------
+
+
+# POST 요청
+
 url = "https://webhook.site/0603ae22-be20-4df5-8929-d265c6af8752"
-querystring = "?name=csw&message=hi"
-headers = {"User-Agent": "csw"}
-res = req.get(url + querystring, headers=headers)
-print(res.status_code)      # 200
-print(res.text)             # ""
+data = {
+    "name": "csw",
+    "message": "hi"
+}
+res = req.post(url, data=data)
+print(res.status_code)
+print(res.text)
