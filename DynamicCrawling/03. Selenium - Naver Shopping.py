3f84e4f88dad46be8334d821e5ad60ae29fe7be8
25th-Night <brown_eyed87@naver.com>
2023-07-16 10:51:43 +0900

    03. Selenium - Naver Shopping - 페이지 이동 (2) - get, back, forward 메서드

--- DynamicCrawling/03. Selenium - Naver Shopping.py
+++ DynamicCrawling/03. Selenium - Naver Shopping.py
@@ -15,17 +15,39 @@ import time
 # Selenium이 Chrome Driver를 만듦
 # Driver가 하나의 프로그램이니까, 프로그램의 위치를 지정해줘야 함
 
-# 브라우저 꺼짐 방지 옵션
+
+# 크롬 브라우저 옵션 지정
 chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+# 브라우저의 창 사이즈 지정
+chrome_options.add_argument("--window-size=900,1000")
+# 샌드박스를 비활성화
+chrome_options.add_argument("--no-sandbox")
+# 크롬을 백그라운드에서 실행
+# chrome_options.add_argument("headless")
+
 
 # 불필요한 에러 메시지 삭제
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
 
 # 크롬 드라이버 최신 버전 설정
 service = Service(executable_path=ChromeDriverManager().install())
 
 
 browser = webdriver.Chrome(service=service, options=chrome_options)
+
+# 페이지 이동
+browser.get("https://naver.com")
+time.sleep(1)
+browser.get("https://shopping.naver.com")
+time.sleep(1)
+
+# 뒤로 가기
+browser.back()
+time.sleep(1)
+
+# 앞으로 가기
+browser.forward()
+
 time.sleep(3)
 browser.close()
